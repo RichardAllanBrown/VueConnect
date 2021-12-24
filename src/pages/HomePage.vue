00e851a1dcc980358860795c7507ff25e9815bc3
Richard Brown <richard.allan.brown@gmail.com>
2021-12-24 08:44:20 +0000

    Restyled home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,9 @@
 
 <template>
     <div class="md:container md:mx-auto px-4">
-        <h1>Vue Connect</h1>
-        <p>A simple replica of the wall from only connect. Select a wall and try to solve it before the time runs out.</p>
-        <ul class="list-disc">
+        <h1 class="text-2xl m-2">Vue Connect</h1>
+        <p class="m-2">A simple replica of the wall from only connect. Select a wall and try to solve it before the time runs out.</p>
+        <ul class="mx-8 list-disc">
             <li v-for="w in walls">
                 <router-link class="no-underline hover:underline text-purple-600" 
                             :to="{ name: 'wall', params: { wallId: w.id } }">{{ w.name }}</router-link>
